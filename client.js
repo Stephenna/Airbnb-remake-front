59bdad99105e9a8e6ed0155716b9a7ea2a748390
Stephenna J Corry <Sjcstephenna@gmail.com>
2021-06-15 19:31:31 -0500

    first commit

---
+++ client.js
@@ -0,0 +1,7 @@
+// import sanityClient from '@sanity/client';
+
+// export default sanityClient({
+//   projectId: 'djzwah2a',
+//   dataset: 'production',
+//   useCdn: true,
+// });
---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import { sanityClient } from '../sanity'
+
+const Home = ({ properties }) => {
+  console.log(properties)
+  return (
+    <h1>Hi there</h1>
+  )
+}
+export const getServerSideProps = async () => {
+  const query = '*[_type == "property"]';
+  const properties = await sanityClient.fetch(query);
+
+  if(!properties.length){
+    return{
+      props: {
+        properties: [],
+      },
+    }
+  } else {
+    return {
+      props: {
+        properties
+      }
+    }
+  }
+
+}
+
+export default Home
